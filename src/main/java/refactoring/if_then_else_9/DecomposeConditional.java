8c59b9a532f7c251272cf1bff083f6424602e6bf
ShotaroSuzu <cosmos.1220.3110@gmail.com>
2021-07-06 10:26:25 +0900

    Chapter9 Decompose Conditional

---
+++ src/main/java/refactoring/if_then_else_9/DecomposeConditional.java
@@ -0,0 +1,21 @@
+package refactoring.if_then_else_9;
+
+import java.util.Date;
+
+public class DecomposeConditional {
+	private Date summerStart = new Date();
+	private Date summerEnd = new Date();
+	private long winterRate;
+	private long winterServiceCharge;
+	private long summerRate;
+
+	public long calcFee(Date date, long quantity) {
+		long charge = 0;
+		if(date.before(summerStart) | date.after(summerEnd)) {
+			charge = quantity * winterRate + winterServiceCharge;
+		} else {
+			charge = quantity * summerRate;
+		}
+		return charge;
+	}
+ }
