46d7ec71873cb467fbf1a5790e4dbf1cd4c5f034
ShotaroSuzu <cosmos.1220.3110@gmail.com>
2021-07-07 09:32:57 +0900

    Chapter9 Refactor Consolidate Conditional ..

--- src/main/java/refactoring/if_then_else_9/ConsolidateConditionalExpression_refactored.java
+++ src/main/java/refactoring/if_then_else_9/ConsolidateConditionalExpression_refactored.java
@@ -1,28 +1,36 @@
 package refactoring.if_then_else_9;
 
+/**
+ * 条件記述の統合
+ * ・同じ結果を保つ一連の条件判定を一つにまとめる。
+ */
 public class ConsolidateConditionalExpression_refactored {
 	private int seniority;
 	private int monthsDisabled;
 	private boolean isPartTime;
 
 	double disabilityAmount() {
-		if (seniority < 2) return 0;
-		if (monthsDisabled > 12) return 0;
-		if(isPartTime) return 0;
+		if(isPartTime()) return 0;
 		//傷病給付金を計算する
 		double amount = 0;
 		/* 省略 */
 		return amount;
 	}
 
+	private boolean isPartTime() {
+		return seniority < 2 || monthsDisabled > 12 || isPartTime;
+	}
+
 	//and条件の例
 	double calcRate() {
-		if(onVacation()) {
-			if(lengthOfService() > 10) {
-				return 1;
-			}
+		if(isFullRate()) {
+			return 1;
 		}
-		return 0.5;
+		return 0.5; // isFullRate() ? 1 : 0.5;
+	}
+	
+	boolean isFullRate() {
+		return onVacation() && lengthOfService() > 10;
 	}
 
 	private int lengthOfService() {
@@ -34,6 +42,7 @@ public class ConsolidateConditionalExpression_refactored {
 		//省略
 		return false;
 	}
+
 
 	
 }
